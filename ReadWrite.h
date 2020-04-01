71b866b4688d6aed4820dd2b0b3132ba90a43ea0
Alex <alextvrus@gmail.com>
2020-04-01 20:14:56 +0300

    Add required headers

---
+++ ReadWrite.h
@@ -0,0 +1,8 @@
+//
+// Created by Alex Glushko on 01.04.2020.
+//
+
+#ifndef KDZ_ARCHIVER_READWRITE_H
+#define KDZ_ARCHIVER_READWRITE_H
+
+#endif //KDZ_ARCHIVER_READWRITE_H
---
+++ Timer.h
@@ -0,0 +1,8 @@
+//
+// Created by Alex Glushko on 01.04.2020.
+//
+
+#ifndef KDZ_ARCHIVER_TIMER_H
+#define KDZ_ARCHIVER_TIMER_H
+
+#endif //KDZ_ARCHIVER_TIMER_H
